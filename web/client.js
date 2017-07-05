7f7b1c82ba79864ae0cd2295398966a201df4345
zsxneil <zsxneil@hotmail.com>
2017-07-05 17:49:05 +0800

    ...

---
+++ web/client.js
@@ -0,0 +1,28 @@
+var http = require('http')
+
+//用于请求的选项
+var options = {
+    host:'localhost',
+    port:'8081',
+    path:'/index.htm'
+}
+
+//处理响应的回调函数
+var callback = (response)=>{
+    //不断更新数据
+    var body = ''
+    response.on('data',(data)=>{
+        body += data;
+    })
+
+    response.on('end',()=>{
+        // 数据接收完成
+        console.log(body)
+    })
+
+    
+}
+
+//向服务端发送请求
+    var req = http.request(options,callback);
+    req.end()
